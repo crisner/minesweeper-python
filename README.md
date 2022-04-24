eb3508e08c9965c406c4d8989f78c7494f9cac76
Renisha Christie <christie.reni@gmail.com>
2022-04-24 23:34:04 +0530

    Create cells

---
+++ README.md
@@ -0,0 +1,1 @@
+Follow along Python Game Development Project Using OOP from [freecodecamp](https://youtu.be/OqbGRZx4xUc).
--- main.py
+++ main.py
@@ -1,4 +1,5 @@
 from tkinter import *
+from cell import Cell
 import settings
 import utils 
 from importlib import reload # reload 
@@ -12,11 +13,20 @@ root.geometry(f'{settings.WIDTH}x{settings.HEIGHT}')
 root.title('Minesweeper')
 root.resizable(False, False)
 
-top_frame = Frame(root, bg='red', width=settings.WIDTH, height=utils.height_perc(25))
+top_frame = Frame(root, bg='black', width=settings.WIDTH, height=utils.height_perc(25))
 top_frame.place(x=0, y=0)
 
-left_frame = Frame(root, bg='green', width=utils.width_perc(25), height=utils.height_perc(75))
+left_frame = Frame(root, bg='black', width=utils.width_perc(25), height=utils.height_perc(75))
 left_frame.place(x=0, y=utils.height_perc(25))
 
+center_frame = Frame(root, bg='black', width=utils.width_perc(75), height=utils.height_perc(75))
+center_frame.place(x=utils.width_perc(25), y=utils.height_perc(25))
+
+for x in range(settings.GRID_SIZE):
+    for y in range(settings.GRID_SIZE):
+        c1 = Cell()
+        c1.create_btn_object(center_frame)
+        c1.cell_btn_object.grid(row=x, column=y)
+        
 # Run/open window
 root.mainloop()
